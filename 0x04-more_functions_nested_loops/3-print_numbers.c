21194353b00457385ad1c45da00100c11c189caa
Joseph Caleb Johnston <josephcalebjohnston@gmail.com>
2023-03-24 11:36:52 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print number from 0 to 9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
