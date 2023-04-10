44f570d5787b698e61d0c55e912242a00a4d7e6b
Joseph Caleb Johnston <josephcalebjohnston@gmail.com>
2023-04-10 07:18:44 -0700

    argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,11 @@
  *main-a program that prints its name, followed by a new line.
  *@argc-arguments count
  *@argv-argument vector
- *
  * Return: 0(success)
  */
 int main(int argc __attribute__((unused)), char **argv)
 {
 	printf("%s\n", *argv);
+
 	return (0);
 }
