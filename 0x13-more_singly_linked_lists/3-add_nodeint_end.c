298a0eec03dd2f54004de1216e9f494efc97d505
Joseph Caleb Johnston <josephcalebjohnston@gmail.com>
2023-05-03 12:58:50 -0700

    betty

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - a function that adds a new node at the end of a listint_t list
+ * add_nodeint_end - a function that adds a new node at the
+ * end of a listint_t list
  * @head: pointer to the first element in the list
  * @n: data to insert in the new element
  *
