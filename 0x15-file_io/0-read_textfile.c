963b50c62e6a976752ba55169f941d0c28cfbe9b
Joseph Caleb Johnston <josephcalebjohnston@gmail.com>
2023-07-27 08:02:41 -0700

    committed changes to multiple files to make them betty compliant

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,7 +7,7 @@
  * @filename: text file being read
  * @letters: number of letters to be read
  * Return: w- actual number of bytes read and printed
- * 	0 when function fails or filename is NULL
+ * 0 when function fails or filename is NULL
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
